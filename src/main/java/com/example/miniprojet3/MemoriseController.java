261bf84bcef4d1b3b7b90e7b8cdabee5336820cf
PAIVANicolas-3il <75265504+PAIVANicolas@users.noreply.github.com>
2023-02-28 09:16:04 +0100

    ajoutes des fichiers AUTHORS et HELPERS

--- src/main/java/com/example/miniprojet3/MemoriseController.java
+++ src/main/java/com/example/miniprojet3/MemoriseController.java
@@ -3,7 +3,7 @@ package com.example.miniprojet3;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MemoriseController {
     @FXML
     private Label welcomeText;
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.miniprojet3 {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.miniprojet3 to javafx.fxml;
     exports com.example.miniprojet3;
 }
